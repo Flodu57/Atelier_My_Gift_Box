1c35cd6fd11db6009394d5a76fc80e665a9e439d
Florian <florian.cordier2@gmail.com>
2018-11-06 16:22:06 +0100

    t

--- src/views/View.php
+++ src/views/View.php
@@ -4,23 +4,16 @@ namespace mygiftbox\views;
 
 class View{
 
-    protected $header, $menu;
-
-    public function __construct(){
-        $this->header = $this->header();
-        $this->menu = $this->menu();
-    }
-
     public function header(){
         $app = \Slim\Slim::getInstance();
         $link = $app->request()->getUrl() . $app->request()->getRootUri();
-        return <<<END
-            <head>
-                <meta charset='UTF-8'>
-                <link rel='stylesheet' href='$link/assets/css/style.css'>
-                <title>MyGiftBox</title>
-            </head>
-END;
+        return "
+                <head>
+                    <meta charset='UTF-8'>
+                    <link rel='stylesheet' href='$link/assets/css/style.css'>
+                    <title>MyGiftBox</title>
+                </head>
+            ";
     }
 
     public function menu(){
