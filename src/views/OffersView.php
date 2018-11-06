b10dac01e934a633f89b60c6007aca903624aeab
Alexduqeb <alexandre.ralli8@etu.univ-lorraine.fr>
2018-11-06 14:29:29 +0100

    Modifs vue + style offres

--- src/views/OffersView.php
+++ src/views/OffersView.php
@@ -17,19 +17,15 @@ class OffersView extends View{
                     <div class='container'>
                         
                         $menu
-                        <div class='register'> 
-                            <p class='label label_nom'>Nooooom</p>
-                            <input type='text' class='input input_nom'>
-                            <p class='label label_prenom'>Prénom</p>
-                            <input type='text' class='input input_prenom'>
-                            <p class='label label_email'>Email</p>
-                            <input type='text' class='input input_email'>
-                            <p class='label label_password'>Mot de passe</p>
-                            <input type='text' class='input input_password'>
-                            <div>
-                                <a href=' class='label label_alreadyRegister'>Déjà inscrit ?</a>
-                                <button type='submit' class='button button_login'>Register</button>
-                            </div>
+                        <div class='offers'> 
+                            <a href='#' class='offer'>
+                                <img src='../../assets/img/diner.jpg'>
+                                <h2>Prestations</h2>
+                                <div class='offer_info'>
+                                    <p>Catégorie</p>
+                                    <p>XX €</p>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </body>
