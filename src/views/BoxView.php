e7eac14b1cc95c451c217c1f49d6a082c66ede48
Florian <florian.cordier2@gmail.com>
2018-11-07 14:23:01 +0100

    Affichage des prestations d'une box

--- src/views/BoxView.php
+++ src/views/BoxView.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace mygiftbox\views;
+use mygiftbox\models\Prestation;
 
 class BoxView extends View{
 
@@ -12,9 +13,28 @@ class BoxView extends View{
     }
 
     public function render(){
+        $link = $this->getLink();
         $error = parent::error();
         $titre = $this->box->titre;
         $total = $this->box->prix_total;
+
+
+        $pres = "";
+        $offers = $this->box->prestations()->get();
+        foreach($offers as $offer) {
+            $pres .= <<<END
+            <a href='#' class='offer'>
+                <img src='$link/assets/img/prestations/$offer->image'>
+                <div class='offer_bottom'>
+                    <h2>$offer->titre</h2>
+                    <div class='offer_bottom_infos'>
+                        <p>$offer->categorie_id</p>
+                        <p>$offer->prix €</p>
+                    </div>
+                </div>
+            </a>
+END;
+        }
 
         $html = <<<END
             <html>
@@ -32,6 +52,10 @@ class BoxView extends View{
                                 <p>€</p>
                             </div>
                           </div>  
+
+                          <div class='box_grid'>
+                            $pres 
+                          </div>
                         </div>
 
                         $this->footer
