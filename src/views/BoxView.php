aa848472faa82a86b71b3fb886564bf9fecf16ef
Florian <florian.cordier2@gmail.com>
2018-11-07 15:22:29 +0100

    Ajout affichage des items d'une box + css

--- src/views/BoxView.php
+++ src/views/BoxView.php
@@ -18,6 +18,9 @@ class BoxView extends View{
         $titre = $this->box->titre;
         $total = $this->box->prix_total;
 
+        $app = \Slim\Slim::getInstance();
+
+        $urlOffers = $app->urlFor('offers');
 
         $pres = "";
         $offers = $this->box->prestations()->get();
@@ -26,10 +29,10 @@ class BoxView extends View{
             <a href='#' class='offer'>
                 <img src='$link/assets/img/prestations/$offer->image'>
                 <div class='offer_bottom'>
-                    <h2>$offer->titre</h2>
+                    <h2 class='label label_title'>$offer->titre</h2>
                     <div class='offer_bottom_infos'>
-                        <p>$offer->categorie_id</p>
-                        <p>$offer->prix €</p>
+                        <p class='label label_category'>$offer->categorie_id</p>
+                        <p class='label label_price'>$offer->prix €</p>
                     </div>
                 </div>
             </a>
@@ -44,18 +47,22 @@ END;
                         $this->menu
                         
                         <div class='box'>
-                          <div class='box_head'>
-                            <h2>$titre</h2>
-                            <div class='box_head_total'>
-                                <p class='p_total'>Total </p>
-                                $total
-                                <p>€</p>
-                            </div>
-                          </div>  
+                            <div class='box_head'>
+                                <h2>$titre</h2>
+                                <div class='box_head_total'>
+                                    <p class='p_total'>Total </p>
+                                    $total
+                                    <p>€</p>
+                                </div>
+                            </div>  
 
-                          <div class='box_grid'>
-                            $pres 
-                          </div>
+                            <div class='box_grid'>
+                                $pres 
+                            </div>
+                            <div class='buttonLayout'>
+                                <a href='$urlOffers' class='button button_continueBox'>Continuer les achats</a>
+                                <a href='#' class='button button_validateBox'>Passer au payement</a>
+                            </div>
                         </div>
                         $this->footer
                     </div>
