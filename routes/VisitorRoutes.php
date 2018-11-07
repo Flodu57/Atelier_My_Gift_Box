8e356f2d39c2f145f60fc83f72c3d4e7ad3d192a
zink anthony <anthony.zink@outlook.fr>
2018-11-07 20:45:46 +0100

    wait for box

--- routes/VisitorRoutes.php
+++ routes/VisitorRoutes.php
@@ -4,16 +4,21 @@ use mygiftbox\controllers\VisitorController;
 use mygiftbox\models\Box;
 
 function CheckOpeningDate($route){
-
-    $box = Box::byToken($route->getParam('token'));
+    $token = $route->getParam('token');
+    $box = Box::byToken($token);
     $date_ouverture = new DateTime($box->date_ouverture);
     $app = \Slim\Slim::getInstance();
     if($date_ouverture > new DateTime('now')){
-        $app->redirect($app->urlFor('home'));
+        $app->redirect($app->urlFor('visitor.wait', compact('token')));
     }
 }
 
 $app->get('/:token', 'CheckOpeningDate', function($token){
     $c = new VisitorController();
     $c->getBoxVisitor($token);
-});
+})->name('visitor.token');
+
+$app->get('/wait/:token', function($token){
+    $c = new VisitorController();
+    $c->getWait($token);
+})->name('visitor.wait');
--- src/controllers/VisitorController.php
+++ src/controllers/VisitorController.php
@@ -3,6 +3,7 @@
 namespace mygiftbox\controllers;
 
 use mygiftbox\views\VisitorBoxView;
+use mygiftbox\views\VisitorWaitView;
 use mygiftbox\models\Box;
 
 class VisitorController {
@@ -10,6 +11,12 @@ class VisitorController {
     public function getBoxVisitor($token){
         $box = Box::byToken($token);
         $v = new VisitorBoxView($box);
+        $v->render();
+    }
+
+    public function getWait($token){
+        $box = Box::byToken($token);
+        $v = new VisitorWaitView($box);
         $v->render();
     }
 
