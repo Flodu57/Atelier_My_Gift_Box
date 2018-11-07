467b4112833ea4a9947f2a6514df2dea1c2b95bd
zink anthony <anthony.zink@outlook.fr>
2018-11-07 19:44:39 +0100

    ajout du middleware pour la date d'ouverture

--- routes/VisitorRoutes.php
+++ routes/VisitorRoutes.php
@@ -1,8 +1,19 @@
 <?php
 
 use mygiftbox\controllers\VisitorController;
+use mygiftbox\models\Box;
 
-$app->get('/:token', function($token){
+function CheckOpeningDate($route){
+
+    $box = Box::byToken($route->getParam('token'));
+    $date_ouverture = new DateTime($box->date_ouverture);
+    $app = \Slim\Slim::getInstance();
+    if($date_ouverture > new DateTime('now')){
+        $app->redirect($app->urlFor('home'));
+    }
+}
+
+$app->get('/:token', 'CheckOpeningDate', function($token){
     $c = new VisitorController();
     $c->getBoxVisitor($token);
 });
