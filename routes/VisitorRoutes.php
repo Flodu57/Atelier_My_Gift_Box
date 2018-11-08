95cb3bfc139e45a066f74311ac408eef658d44c3
zink anthony <anthony.zink@outlook.fr>
2018-11-08 08:43:45 +0100

    gestion erreur token

--- routes/VisitorRoutes.php
+++ routes/VisitorRoutes.php
@@ -4,10 +4,14 @@ use mygiftbox\controllers\VisitorController;
 use mygiftbox\models\Box;
 
 function CheckOpeningDate($route){
+    $app = \Slim\Slim::getInstance();
     $token = $route->getParam('token');
     $box = Box::byToken($token);
+    
+    if(!$box)
+        $app->redirect($app->urlFor('home'));
+    
     $date_ouverture = new DateTime($box->date_ouverture);
-    $app = \Slim\Slim::getInstance();
     if($date_ouverture > new DateTime('now')){
         $app->redirect($app->urlFor('visitor.wait', compact('token')));
     }
